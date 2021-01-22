5c8fd52e11eed2e5140eb16c988e771f19740298
Javier Palacios <javier.palacios@cision.com>
2021-01-22 01:19:13 -0300

    feat: add map direction polyline in the map

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import cn from 'classnames';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import SmartphoneIcon from '@material-ui/icons/Smartphone';
+import EmailIcon from '@material-ui/icons/Email';
 
 import styles from './footer.module.css';
 import logo from '../../assets/jj-logo-white.png';
@@ -15,29 +18,34 @@ const Footer: React.FC = () => {
         <h3 className={styles.title}>Servicios</h3>
         <div className={styles.containerText}>
           <div className={styles.icon}>
-            <ArrowForwardIosIcon />
+            <ArrowForwardIosIcon className={styles.arrowIcon} />
           </div>
           <div className={styles.text}>Impresoras</div>
         </div>
         <div className={styles.containerText}>
           <div className={styles.icon}>
-            <ArrowForwardIosIcon />
+            <ArrowForwardIosIcon className={styles.arrowIcon} />
           </div>
           <div className={styles.text}>Computadores</div>
         </div>
         <div className={styles.containerText}>
           <div className={styles.icon}>
-            <ArrowForwardIosIcon />
+            <ArrowForwardIosIcon className={styles.arrowIcon} />
           </div>
           <div className={styles.text}>Nooteboks</div>
         </div>
       </div>
       <div className={styles.data}>
         <h3 className={styles.title}>Contacta con nosotros</h3>
-        <p className={styles.addresse}>Av. Pablo Neruda #035. Villa España, Melipilla.</p>
         <div className={styles.containerText}>
           <div className={styles.icon}>
-            <ArrowForwardIosIcon />
+            <LocationOnIcon className={styles.contactIcon} />
+          </div>
+          <p className={styles.addresse}>Av. Pablo Neruda #035. Villa España, Melipilla.</p>
+        </div>
+        <div className={styles.containerText}>
+          <div className={styles.icon}>
+            <SmartphoneIcon className={styles.contactIcon} />
           </div>
           <a href="tel:+56978477250" className={styles.text}>
             Teléfono : +569 7847 7250
@@ -45,7 +53,7 @@ const Footer: React.FC = () => {
         </div>
         <div className={styles.containerText}>
           <div className={styles.icon}>
-            <ArrowForwardIosIcon />
+            <EmailIcon className={styles.contactIcon} />
           </div>
           <a href="mailto: gmomel@gmail.com" className={styles.text}>
             E-mail : gmomel@gmail.com
--- src/components/jj-map/jj-map.tsx
+++ src/components/jj-map/jj-map.tsx
@@ -4,19 +4,69 @@ import GoogleMapReact from 'google-map-react';
 import Marker from '../marker';
 import styles from './jj-map.module.css';
 
-const lat = -33.684976;
-const lng = -71.206135;
-
+const DIRECTION_TRAVEL_MODE = 'DRIVING';
+const latitude = -33.684976;
+const longitude = -71.206135;
+const mapConfig = {
+  center: {
+    lat: latitude,
+    lng: longitude,
+  },
+  zoom: 18,
+};
 const Footer: React.FC = () => {
   const GOOGLE_MAP_API_KEY: string = process.env.REACT_APP_GOOGLE_MAP_API_KEY || '';
 
-  const mapConfig = {
-    center: {
-      lat,
-      lng,
-    },
-    zoom: 18,
-  };
+  function setMap({ map, maps }: any) {
+    const directionsService = new maps.DirectionsService();
+    const bounds = new maps.LatLngBounds();
+
+    if (navigator?.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { lat, lng } = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          directionsService.route(
+            {
+              origin: `${lat},${lng}`,
+              destination: `${latitude},${longitude}`,
+              travelMode: DIRECTION_TRAVEL_MODE,
+            },
+            (response: any, status: any) => {
+              if (status === 'OK') {
+                // Get polyline boundaries
+                const boundaries = response.routes[0].bounds;
+                const { Ra, Wa } = boundaries;
+
+                // Create direction line with direction route
+                const routePolyline = new maps.Polyline({
+                  path: response.routes[0].overview_path,
+                  strokeColor: '#ff9800',
+                });
+
+                // Center map in the direction line boundaried
+                bounds.extend(new maps.LatLng(Wa.j, Ra.i));
+                bounds.extend(new maps.LatLng(Wa.i, Ra.j));
+                bounds.extend(new maps.LatLng(latitude, longitude));
+                map.fitBounds(bounds);
+                // Draw direction line in the map
+                routePolyline.setMap(map);
+              } else {
+                console.log(`Directions service failed due to ${status}`);
+              }
+            },
+          );
+        },
+        err => {
+          console.log('Error requesting geolocation');
+        },
+      );
+    } else {
+      console.log('No geolocation');
+    }
+  }
 
   return (
     <div className={styles.jjMap}>
@@ -24,8 +74,9 @@ const Footer: React.FC = () => {
         bootstrapURLKeys={{ key: GOOGLE_MAP_API_KEY }}
         defaultCenter={mapConfig.center}
         defaultZoom={mapConfig.zoom}
+        onGoogleApiLoaded={setMap}
       >
-        <Marker lat={lat} lng={lng} />
+        <Marker lat={latitude} lng={longitude} />
       </GoogleMapReact>
     </div>
   );
